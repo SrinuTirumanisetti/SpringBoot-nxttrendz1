ea5ce4a851695acd710b4960a21d1d32718b32d9
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-08-17 01:09:22 +0000

    Implemented getProducts and addProduct API

--- src/main/java/com/example/nxttrendz1/service/ProductJpaService.java
+++ src/main/java/com/example/nxttrendz1/service/ProductJpaService.java
@@ -1,14 +1,31 @@
-/*
- *
- * You can use the following import statements
- * 
- * import org.springframework.beans.factory.annotation.Autowired;
- * import org.springframework.http.HttpStatus;
- * import org.springframework.stereotype.Service;
- * import org.springframework.web.server.ResponseStatusException;
- * import java.util.ArrayList;
- * import java.util.List;
- * 
- */
+package com.example.nxttrendz1.service;
 
-// Write your code here
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.nxttrendz1.model.Product;
+import com.example.nxttrendz1.repository.ProductRepository;
+import com.example.nxttrendz1.repository.ProductJpaRepository;
+
+@Service
+public class ProductJpaService implements ProductRepository{
+    @Autowired
+    public ProductJpaRepository productJpaRepository;
+
+    @Override
+    public List<Product> getProducts(){
+        List<Product> products = productJpaRepository.findAll();
+        return products;
+    }
+
+    @Override
+    public Product addProduct(Product product){
+       return productJpaRepository.save(product);
+    }
+
+    //hello addProduct
+}
